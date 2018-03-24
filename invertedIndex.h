5b6afb983d329ad86ee90b759bdeecb87951001d
fl273 <fan.liu3778@gmail.com>
2018-03-23 21:03:11 -0400

    testplan.txt

--- invertedIndex.h
+++ invertedIndex.h
@@ -1,16 +1,34 @@
 #ifndef INVERTEDINDEX_H_
 #define INVERTEDINDEX_H_
 
- #define BUFSIZE 5000
- int tokenize(char* fileName, char ** wordArr);
- void clearBuffer(char* str, int len);
- node* createNode(char* str, int count, node* next);
- void sort(char** allStrings, int wordCount);
- node* removeDuplicates(char** allwords, int len);
- void printHLL(hNode* head);
- void PrintLL (node* head);
- void traverseDir(hNode** hashTable, char* path);
- void insertRecords(hNode** hashTable, node* head, char* fileName);
- int hashFunction(char* str);
+  #define BUFSIZE 5000
+
+  //record node
+  typedef struct node{
+    char* str;
+    int count;
+    struct node* next;
+  }node;
+  
+  //hash node
+  typedef struct hNode{
+    node*fileList;
+    char* keyword;
+    struct hNode* next;
+  }hNode;
+  
+  typedef struct hNode{
+  int tokenize(char* fileName, char ** wordArr);
+  void clearBuffer(char* str, int len);
+  node* createNode(char* str, int count, node* next);
+  void sort(char** allStrings, int wordCount);
+  node* removeDuplicates(char** allwords, int len);
+  void printHLL(hNode* head);
+  void PrintLL (node* head);
+  void traverseDir(hNode** hashTable, char* path);
+  void insertRecords(hNode** hashTable, node* head, char* fileName);
+  int hashFunction(char* str);
+  hNode* mergeSortKw(hNode* head);
+  node* mergeSortRecords(node* head);
 
 #endif
